b7d21644e84255c0a7d1e46f1a727ee284d87fe0
Jesse-L2 <jesse.little@utexas.edu>
2024-06-24 15:20:12 -0500

    Start Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useState, useEffect } from "react";
+import api from "../../api";
+
 const Home = () => {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
   return <div>Home</div>;
 };
 
